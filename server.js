e3c77db07f2a480534d0ff589d4294cc5a38860d
overlord <rexatuzie@gmail.com>
2023-01-08 17:10:34 +0100

    trace 89

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const path = require("path");
-
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const errorHandler = require("./middlewares/errorMiddleware");
@@ -13,12 +12,13 @@ const errorHandler = require("./middlewares/errorMiddleware");
 const app = express();
 
 //Middlewares
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 //Routes Middleware
 app.use("/api/auth", authRoutes);
