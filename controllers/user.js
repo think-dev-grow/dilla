4c55b4c9c7ef48fe544f933b0abce9b9622972a6
overlord <rexatuzie@gmail.com>
2023-01-10 18:03:45 +0100

    rfv nf vvhj hj

--- controllers/user.js
+++ controllers/user.js
@@ -25,7 +25,7 @@ const getUser = asyncHandler(async (req, res) => {
     throw new Error("User does not exist.");
   }
 
-  const { _id, email, kodeHex } = user;
+  const { _id, email, kodeHex, verified } = user;
 
   res.status(200).json({
     success: true,
@@ -34,6 +34,7 @@ const getUser = asyncHandler(async (req, res) => {
       _id,
       email,
       kodeHex,
+      verified,
     },
   });
 });
