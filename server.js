02232c2d9fb9e6986e57b194cbb5ab5b5e085533
overlord <rexatuzie@gmail.com>
2023-01-10 17:27:15 +0100

    hvhjb

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const errorHandler = require("./middlewares/errorMiddleware");
 const app = express();
 
 //Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
