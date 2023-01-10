e6f49e03333fed03a75f6bf91e27abaa11ca9895
overlord <rexatuzie@gmail.com>
2023-01-10 07:21:37 +0100

    vmdsv dx,

--- utils/fileUpload.js
+++ utils/fileUpload.js
@@ -1,8 +1,13 @@
 const multer = require("multer");
+const fs = require("fs");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads");
+    // cb(null, "uploads");
+    // cb(null, __dirname);
+    fs.mkdir("./uploads/", (err) => {
+      cb(null, "./uploads/");
+    });
   },
   filename: function (req, file, cb) {
     cb(
