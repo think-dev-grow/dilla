9a600428aaa12fb329665ba1a237d5c9d8bc0d8d
overlord <rexatuzie@gmail.com>
2023-01-11 17:37:16 +0100

    gyfyybnmsbjs

---
+++ models/FlexPlan.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const randomize = require("randomatic");
+
+const FlexSchema = new mongoose.Schema(
+  {
+    userID: { type: String },
+    type: { type: String, default: "auto" },
+    name: { type: String },
+    earn: { type: Number, default: 0 },
+    exp: { type: Number, default: 0 },
+    accountBalance: { type: Number, default: 0 },
+    accountNumber: { type: String, default: randomize(0, 10) },
+    autoSavingTarget: { type: Number, default: 0 },
+    autoSavingRate: { type: Number, default: 0 },
+    autoDuration: { type: String },
+    customSavingTarget: { type: Number, default: 0 },
+    customSavingRate: { type: Number, default: 0 },
+    customDuration: { type: String },
+    psr: { type: Array },
+    cPsr: { type: Array },
+    badge: { type: String, default: "cadet" },
+    savingPeriod: { type: String, default: "" },
+    totalIntrest: { type: Number, default: 0 },
+    intrestPerMonth: { type: Array },
+    breakdown: { type: Array },
+    paymentDate: { type: String },
+    activatePlan: { type: Boolean, default: false },
+    transcactionHistory: { type: Array, default: [] },
+  },
+  { timestamps: true }
+);
+
+const FlexPlan = mongoose.model("FlexPlan", FlexSchema);
+module.exports = FlexPlan;
