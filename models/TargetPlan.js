735621e257b7c534a043d81115247eb61f404d79
overlord <rexatuzie@gmail.com>
2023-01-12 11:29:05 +0100

    nbvhvhh

--- models/TargetPlan.js
+++ models/TargetPlan.js
@@ -9,7 +9,7 @@ const TargetSchema = new mongoose.Schema(
     earn: { type: Number, default: 0 },
     exp: { type: Number, default: 0 },
     accountBalance: { type: Number, default: 0 },
-    accountNumber: { type: String, default: randomize(0, 10) },
+    accountNumber: { type: Number, default: randomize(0, 10) },
     autoSavingTarget: { type: Number, default: 0 },
     autoSavingRate: { type: Number, default: 0 },
     autoDuration: { type: String },
