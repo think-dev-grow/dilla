03b4451449b39d7e3be5fbf4fdf25fff02e38ba0
overlord <rexatuzie@gmail.com>
2023-01-19 11:14:53 +0100

    thu commit 10

--- routes/user.js
+++ routes/user.js
@@ -37,7 +37,7 @@ router.post("/id-front", protect, upload.single("image"), uploadIdFront);
 
 router.post("/id-back", protect, upload.single("image"), uploadIdBack);
 
-router.grt("/total-balance", protect, calculateTotalBalance);
+router.get("/total-balance", protect, calculateTotalBalance);
 
 router.post(
   "/utility-bill",
